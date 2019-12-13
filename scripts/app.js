28781e0e6d9586decbedaa89c7b44c902a89260a
Dan Ochs <dochs@ochsholdings.com>
2019-12-13 13:36:51 -0600

    updated player app to match main trivia app

--- scripts/app.js
+++ scripts/app.js
@@ -11,8 +11,7 @@ const a4 = document.getElementById('a4');
 let pin = '';
 let gameRef = {};
 let playerRef = {};
-// TODO: playerObj needs dynamic initialization based on number of questions
-let playerObj = {'name': '', '0': -1, '1': -1, '2': -1, '3': -1, '4': -1, '5': -1, '6': -1, '7': -1, '8': -1, '9': -1};
+let playerObj = {};
 let qindex = -1;
 
 joinButton.addEventListener('click', ()=>{
@@ -30,6 +29,11 @@ joinButton.addEventListener('click', ()=>{
                 nameTextbox.style.display = 'inline-block';
                 submitButton.style.display = 'inline-block';
                 statusLabel.innerHTML = 'Enter name and press submit';
+                playerObj = {'name': 'zzz'};
+                for (let i = 0; i < snapshot.val().question.num; i++) {
+                    let key = i.toString();
+                    playerObj[key] = [0, -1];
+                }
             }
             else {
                 statusLabel.innerHTML = 'Game not available to join. try again...';
@@ -52,44 +56,141 @@ submitButton.addEventListener('click', ()=>{
     playerRef = gameRef.child('players').push(playerObj);
     nameTextbox.style.display = 'none';
     submitButton.style.display = 'none';
-    a1.style.display = 'inline-block';
-    a2.style.display = 'inline-block';
-    a3.style.display = 'inline-block';
-    a4.style.display = 'inline-block';
+
+    a1.style.display = 'none';
+    a2.style.display = 'none';
+    a3.style.display = 'none';
+    a4.style.display = 'none';
+    a1.style.backgroundColor = 'steelblue';
+    a2.style.backgroundColor = 'steelblue';
+    a3.style.backgroundColor = 'steelblue';
+    a4.style.backgroundColor = 'steelblue';
 
     // set a question listener
     gameRef.child('question').on('value', (snapshot) => {updateQ(snapshot)});
 });
 
 function updateQ(snapshot) {
-    statusLabel.innerHTML = snapshot.val().text;
-    qindex = snapshot.val().qindex;
+    
+    if (snapshot.val().text == 'JOINING') {
+        statusLabel.innerHTML = 'Waiting for game to start...';
+        qindex = -1;
+    }
+    else if (snapshot.val().text == 'GAME_OVER') {
+        statusLabel.innerHTML = snapshot.val().text;
+        qindex = -1;
+        a1.style.display = 'none';
+        a2.style.display = 'none';
+        a3.style.display = 'none';
+        a4.style.display = 'none';
+    }
+    else {
+        statusLabel.innerHTML = snapshot.val().text;
+        qindex = snapshot.val().num;
+        // turn on the correct number of buttons
+        if (snapshot.val().ans == 2) {
+            a1.style.backgroundColor = 'steelblue';
+            a2.style.backgroundColor = 'steelblue';
+            a3.style.backgroundColor = 'steelblue';
+            a4.style.backgroundColor = 'steelblue';
+            a1.style.display = 'inline-block';
+            a2.style.display = 'inline-block';
+            a3.style.display = 'none';
+            a4.style.display = 'none';
+        }
+        if (snapshot.val().ans == 3) {
+            a1.style.backgroundColor = 'steelblue';
+            a2.style.backgroundColor = 'steelblue';
+            a3.style.backgroundColor = 'steelblue';
+            a4.style.backgroundColor = 'steelblue';
+            a1.style.display = 'inline-block';
+            a2.style.display = 'inline-block';
+            a3.style.display = 'inline-block';
+            a4.style.display = 'none';
+        }
+        if (snapshot.val().ans == 4) {
+            a1.style.backgroundColor = 'steelblue';
+            a2.style.backgroundColor = 'steelblue';
+            a3.style.backgroundColor = 'steelblue';
+            a4.style.backgroundColor = 'steelblue';
+            a1.style.display = 'inline-block';
+            a2.style.display = 'inline-block';
+            a3.style.display = 'inline-block';
+            a4.style.display = 'inline-block';
+        }
+    }
 }
 
-a1.addEventListener('click', () => {
+// THESE NOW NEED TO CREATE A RESPONSE OBJECT AND POST IT
+
+a1.addEventListener('click', (ev) => {
     if (qindex != -1) {
-        playerObj[(qindex).toString()] = 1;
-        playerRef.set(playerObj);
+        // create a response object
+        let response = {'p': playerRef.key, 'q': qindex, 'r': 0};
+
+        // record this response in the responses list
+        gameRef.child('responses').push(response);
+
+        // response entered - disable the buttons
+        ev.target.style.backgroundColor = 'red';
+
+        //let buttons = answers.children;
+        //for (let i=0; i<buttons.length; i++) {
+        //    buttons[i].firstChild.disabled = 'true';
+        //}
     }
 });
 
-a2.addEventListener('click', () => {
+a2.addEventListener('click', (ev) => {
     if (qindex != -1) {
-        playerObj[(qindex).toString()] = 2;
-        playerRef.set(playerObj);
+        // create a response object
+        let response = {'p': playerRef.key, 'q': qindex, 'r': 1};
+
+        // record this response in the responses list
+        gameRef.child('responses').push(response);
+
+        // response entered - disable the buttons
+        ev.target.style.backgroundColor = 'red';
+
+        //let buttons = answers.children;
+        //for (let i=0; i<buttons.length; i++) {
+        //    buttons[i].firstChild.disabled = 'true';
+        //}
     }
 });
 
-a3.addEventListener('click', () => {
+a3.addEventListener('click', (ev) => {
     if (qindex != -1) {
-        playerObj[(qindex).toString()] = 3;
-        playerRef.set(playerObj);
+        // create a response object
+        let response = {'p': playerRef.key, 'q': qindex, 'r': 2};
+
+        // record this response in the responses list
+        gameRef.child('responses').push(response);
+
+        // response entered - disable the buttons
+        ev.target.style.backgroundColor = 'red';
+
+        //let buttons = answers.children;
+        //for (let i=0; i<buttons.length; i++) {
+        //    buttons[i].firstChild.disabled = 'true';
+        //}
     }
 });
 
-a4.addEventListener('click', () => {
+a4.addEventListener('click', (ev) => {
     if (qindex != -1) {
-        playerObj[(qindex).toString()] = 4;
-        playerRef.set(playerObj);
+        // create a response object
+        let response = {'p': playerRef.key, 'q': qindex, 'r': 3};
+
+        // record this response in the responses list
+        gameRef.child('responses').push(response);
+
+        // response entered - disable the buttons
+        ev.target.style.backgroundColor = 'red';
+
+        //let buttons = answers.children;
+        //for (let i=0; i<buttons.length; i++) {
+        //    buttons[i].firstChild.disabled = 'true';
+        //}
     }
 });
